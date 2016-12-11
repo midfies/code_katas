cac1c70edc84813bfc5e5448fae328247be255c7
Marc Fieser <midfies@gmail.com>
2016-12-11 13:23:45 -0800

    fixed errors

--- src/sum_of_nth.py
+++ src/sum_of_nth.py
@@ -3,9 +3,7 @@
 
 def series_sum(n):
     """Return the nth term of the series: 1 + 1/4 + 1/7 + 1/10 + 1/13."""
-    sum = 0
+    total = 0
     for i in range(n):
-        sum += 1.0 / (3 * i + 1)
-    return '%.2f' % sum
-
-print(series_sum(5))
+        total += 1.0 / (3 * i + 1)
+    return '%.2f' % total
